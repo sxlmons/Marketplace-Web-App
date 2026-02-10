0207056f05cac3c78151cf85c194c58c00b61e95
CampbellWest <campbellwest1@gmail.com>
2026-02-10 11:23:28 -0500

    Updating Migrations

---
+++ MarketPlaceBackend/MarketPlaceBackend/Migrations/20260210015706_NewIntialTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketPlaceBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewIntialTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ MarketPlaceBackend/MarketPlaceBackend/Migrations/20260210020255_IntitialAppTables.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketPlaceBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntitialAppTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
